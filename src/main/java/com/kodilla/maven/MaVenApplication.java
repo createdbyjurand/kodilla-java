f1e6dcf58e806e62d2a47da39917da79cfd0eec5
createdbyjurand <createdbyjurand@gmail.com>
2022-05-30 01:37:17 +0200

    Modul_07 spring boot

---
+++ src/main/java/com/kodilla/maven/MaVenApplication.java
@@ -0,0 +1,12 @@
+package com.kodilla.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaVenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaVenApplication.class, args);
+    }
+}
---
+++ src/test/java/com/kodilla/maven/MaVenApplicationTests.java
@@ -0,0 +1,12 @@
+package com.kodilla.maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaVenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
